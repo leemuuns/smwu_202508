94dd4ae0814050e4578e1c51955cf784cf2210ba
lizlim <myliz.lim@gmail.com>
2025-08-19 16:43:51 +0900

    column_practice_screen.dart 화면 추가

--- lib/screen/button_screen.dart
+++ lib/screen/button_screen.dart
@@ -3,6 +3,7 @@ import 'package:smwu_202508/screen/column/column_practice_screen.dart';
 import 'package:smwu_202508/screen/column/column_screen.dart';
 import 'package:smwu_202508/screen/container/container_practice_screen.dart';
 import 'package:smwu_202508/screen/container/container_screen.dart';
+import 'package:smwu_202508/screen/row/row_screen.dart';
 
 class ButtonScreen extends StatelessWidget {
   const ButtonScreen({super.key});
@@ -36,7 +37,12 @@ class ButtonScreen extends StatelessWidget {
                 return ColumnPracticeScreen();
               },));
             }, child: Text("Column 실습")),
-
+            SizedBox(height: 10),
+            ElevatedButton(onPressed: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return RowScreen();
+              },));
+            }, child: Text("Row")),
 
           ])),
     );
