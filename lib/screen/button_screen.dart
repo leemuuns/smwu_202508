3b3d9e54e8272e028fc06a3637b2119bb0ec9e91
lizlim <myliz.lim@gmail.com>
2025-08-20 16:16:15 +0900

    stack_screen.dart 추가

--- lib/screen/button_screen.dart
+++ lib/screen/button_screen.dart
@@ -8,6 +8,7 @@ import 'package:smwu_202508/screen/image/image_screen.dart';
 import 'package:smwu_202508/screen/row/column_row_practice_screen.dart';
 import 'package:smwu_202508/screen/row/row_practice_screen.dart';
 import 'package:smwu_202508/screen/row/row_screen.dart';
+import 'package:smwu_202508/screen/stack/stack_screen.dart';
 import 'package:smwu_202508/screen/text/text_practice_screen.dart';
 import 'package:smwu_202508/screen/text/text_screen.dart';
 
@@ -85,6 +86,12 @@ class ButtonScreen extends StatelessWidget {
                 return ImagePracticeScreen();
               },));
             }, child: Text("Image 실습")),
+            SizedBox(height: 10),
+            ElevatedButton(onPressed: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return StackScreen();
+              },));
+            }, child: Text("Stack")),
           ])),
     );
   }
---
+++ lib/screen/stack/stack_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class StackScreen extends StatelessWidget {
+  const StackScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Stack'),),
+      body: SizedBox(
+        width: double.infinity,
+        height: double.infinity,
+        child: Stack( //스택은 위젯 겹치기 가능. 먼저 그려진 애들이 더 아래 위치
+          children: [
+            Positioned(
+                top: 0, left: 0,
+                child: Container(color: Colors.purple, width: 170, height: 170,)),
+            Positioned(
+                top: 40, left: 40,
+                child: Container(color: Colors.yellow, width: 160, height: 160,)),
+            Positioned(
+                top: 100, left: 100,
+                child: Container(color: Colors.orange, width: 120, height: 120,)),
+            Positioned(
+                top: 170, left: 170,
+                child: Container(color: Colors.red, width: 70, height: 70,)),
+          ],
+        ),
+      ),
+    );
+  }
+}
