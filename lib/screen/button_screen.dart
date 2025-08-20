24d34e707375efeef9b48b11542b6da8fe5097bc
lizlim <myliz.lim@gmail.com>
2025-08-20 14:58:25 +0900

    row_practice_screen.dart 화면 추가

--- lib/screen/button_screen.dart
+++ lib/screen/button_screen.dart
@@ -3,8 +3,11 @@ import 'package:smwu_202508/screen/column/column_practice_screen.dart';
 import 'package:smwu_202508/screen/column/column_screen.dart';
 import 'package:smwu_202508/screen/container/container_practice_screen.dart';
 import 'package:smwu_202508/screen/container/container_screen.dart';
+import 'package:smwu_202508/screen/row/column_row_practice_screen.dart';
 import 'package:smwu_202508/screen/row/row_practice_screen.dart';
 import 'package:smwu_202508/screen/row/row_screen.dart';
+import 'package:smwu_202508/screen/text/text_practice_screen.dart';
+import 'package:smwu_202508/screen/text/text_screen.dart';
 
 class ButtonScreen extends StatelessWidget {
   const ButtonScreen({super.key});
@@ -50,6 +53,24 @@ class ButtonScreen extends StatelessWidget {
                 return RowPracticeScreen();
               },));
             }, child: Text("Row 실습")),
+            SizedBox(height: 10),
+            ElevatedButton(onPressed: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return ColumnRowPracticeScreen();
+              },));
+            }, child: Text("Column, Row 실습")),
+            SizedBox(height: 10),
+            ElevatedButton(onPressed: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return TextScreen();
+              },));
+            }, child: Text("Text")),
+            SizedBox(height: 10),
+            ElevatedButton(onPressed: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return TextPracticeScreen();
+              },));
+            }, child: Text("Text 실습")),
           ])),
     );
   }
